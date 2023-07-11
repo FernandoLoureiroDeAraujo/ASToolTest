2c20df3834c9cae6ed21d35964f13d2f6e26acbb
fernando <fernandoaraujo402@gmail.com>
2023-07-11 07:39:14 -0300

    file1: if

--- file1.c
+++ file1.c
@@ -4,5 +4,13 @@ int main() {
 
     printf("Hello, World!\n");
 
+    int contador = 0;
+
+    if (contador == 0) {
+        printf("O contador é igual a zero.\n");
+    } else {
+        printf("O contador não é igual a zero.\n");
+    }
+
     return 0;
 }
